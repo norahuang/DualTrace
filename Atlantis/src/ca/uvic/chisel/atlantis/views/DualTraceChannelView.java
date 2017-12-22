00189874ec53018db65d88b53fb26d617a07ca1c
norahuang <vac48@hotmail.com>
2017-12-22 11:43:34 -0800

    update

--- Atlantis/src/ca/uvic/chisel/atlantis/views/DualTraceChannelView.java
+++ Atlantis/src/ca/uvic/chisel/atlantis/views/DualTraceChannelView.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
